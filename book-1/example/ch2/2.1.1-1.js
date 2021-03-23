80402cbd3968123aa45d6b4cb2358af268167424
smw0807 <smw0807@gmail.com>
2021-03-23 22:01:27 +0900

    book-1 > 조금 밖에 안읽음

--- book-1/example/ch2/2.1.1-1.js
+++ book-1/example/ch2/2.1.1-1.js
@@ -2,8 +2,14 @@ if (true) {
   var x = 3;
 }
 console.log(x); // 3
-
+/**
+ * var는 함수 스코프를 가지고 있어 if의 블록과 상관 없이 접근이 가능
+ */
 if (true) {
   const y = 3;
 }
 console.log(y); // Uncaught ReferenceError: y is not defined
+/**
+ * const와 let은 블록 스코프를 가지고 있어 블록 밖에서는 접근이 불가능 함
+ * 또한 const는 한 번 값을 할당하면 다른 값을 할당할 수 없음
+ */
