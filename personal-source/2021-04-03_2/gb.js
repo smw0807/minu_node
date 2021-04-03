1568f002f7b059cf8aff8b00dbd324b9c67e0fc0
smw0807 <smw0807@gmail.com>
2021-04-03 16:44:10 +0900

    bool1 > console 객체

---
+++ personal-source/2021-04-03_2/gb.js
@@ -0,0 +1,18 @@
+console.log(global);
+/**
+ * 출력 결과물
+Object [global] {
+  global: [Circular],
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Function]
+  },
+  queueMicrotask: [Function: queueMicrotask],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Function]
+  }
+}
+ */
