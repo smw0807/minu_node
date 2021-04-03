89b01a8bedd10694fcd1194242a6a66bd0e8eb4a
smw0807 <smw0807@gmail.com>
2021-04-03 14:38:00 +0900

    book-1 > 모둘화

---
+++ personal-source/2021-04-03/md.js
@@ -0,0 +1,43 @@
+// const a = 'a';
+
+// const b = {
+//   bb: 'bb',
+//   cc: 'cc'
+// }
+
+// function c () {
+//   console.log('cc');
+// }
+
+// function d (val) {
+//   console.log(val);
+// }
+
+// module.exports = {
+//   a,
+//   b,
+//   c,
+//   d
+// }
+
+const a = 'a';
+
+const b = {
+  bb: 'bb',
+  cc: 'cc'
+}
+
+function c () {
+  console.log('cc');
+}
+
+function d (val) {
+  console.log(val);
+}
+
+export default {
+  a,
+  b,
+  c,
+  d
+}
---
+++ personal-source/2021-04-03/use.js
@@ -0,0 +1,15 @@
+// const test = require('./md');
+
+// console.log(test.a);
+// console.log(test.b.bb);
+// console.log(test.b.cc);
+// test.c();
+// test.d('zz');
+
+import {a, b, c, d} from './md'
+
+console.log(a);
+console.log(b.bb);
+console.log(c.cc);
+c();
+d('zz');
